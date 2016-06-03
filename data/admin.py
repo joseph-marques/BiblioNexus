3abd4d5502e907db77086f8bbdd17294c3ff38a7
Joseph <joseph@dontcallmejoe.com>
2016-06-03 17:07:04 -0400

    Made enough of the model to make the admin interface usable.

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Book, Series
+
+admin.site.register(Author)
+admin.site.register(Series)
+admin.site.register(Book)
--- data/models.py
+++ data/models.py
@@ -4,19 +4,24 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=128) #The name of the author
 
+    def __str__(self):
+        return self.name
 
 class Series(models.Model):
     title = models.CharField(max_length=512) #the title of the series
     authors = models.ManyToManyField(Author) #the authors that participated in the making of the series
-    books = [] #the list of books that are in the series
-    def AddBook(Book(x)): #a method to get books into the series list
-        books.append(x)
+
+    def __str__(self):
+        return self.title + " by "+" and ".join(str(x) for x in self.authors.all())
 
 
 class Book(models.Model):
     title = models.CharField(max_length=512) #the title of the book
     authors = models.ManyToManyField(Author) #describes the author (or authors) of the book
-    series = models.ForeignKey(Series) #describes what series the book is in, if at all.  will be null if not there
-    seriesSpot = models.IntegerField() #describes which book in the series this is, if 0, no series
+    series = models.ForeignKey(Series, null=True, blank=True) #describes what series the book is in, if at all.  will be null if not there
+    seriesSpot = models.IntegerField(default=0) #describes which book in the series this is, if 0, no series
     publish_date = models.DateField() #states the date that the book was published
+
+    def __str__(self):
+        return self.title + " by "+" and ".join(str(x) for x in self.authors.all())
 
