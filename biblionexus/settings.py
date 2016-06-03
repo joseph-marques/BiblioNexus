66564609a6f35dab7e10994c71ba3b1ae96a7a12
Joseph <joseph@dontcallmejoe.com>
2016-06-03 18:59:51 -0400

    Started the browse template and added in bootstrap and jquery as static files in browse.

--- biblionexus/settings.py
+++ biblionexus/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'data.apps.DataConfig',
+    'browse.apps.BrowseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- browse/views.py
+++ browse/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from data.models import Book
 # Create your views here.
 def index(request):
-    return HttpResponse("This is the browse page.")
+    get_books = Book.objects.order_by("title")
+    context = {
+        'books_to_display': get_books,
+    }
+    return render(request, 'browse/index.html', context)
