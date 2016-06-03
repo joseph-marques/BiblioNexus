9e94e0ad1966edd8c51972752771461ebc73f1bb
cablooshe <cablooshe@gmail.com>
2016-06-03 15:05:59 -0400

    Fixed up how series works.  They now contain a list that should have books added to them. Also since Safari is currently breaking at the time, most of the code is probably wrong.

--- data/models.py
+++ data/models.py
@@ -9,11 +9,14 @@ class Series(models.Model):
     title = models.CharField(max_length=512)
     authors = models.ManyToManyField(Author)
     books = []
+    def AddBook(Book(x)):
+        books.append(x)
 
 
 class Book(models.Model):
     title = models.CharField(max_length=512)
     authors = models.ManyToManyField(Author)
     series = models.ForeignKey(Series)
+    seriesSpot = models.IntegerField()
     publish_date = models.DateField()
 
