136fea6c1e5efd8653327c79364b07e703e45ff0
Joseph <joseph@dontcallmejoe.com>
2016-06-03 15:35:20 -0400

    Slightly improved model structure and url improvements.

--- biblionexus/urls.py
+++ biblionexus/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('browse.urls')),
     url(r'^read/', include('read.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('browse.urls')),
 ]
