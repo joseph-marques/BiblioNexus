2993475ff7ea691be91bca64689a8c70b600a011
Joseph <joseph@dontcallmejoe.com>
2016-06-04 01:25:30 -0400

    Added some more structure to the data module.

--- data/urls.py
+++ data/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.add, name='add'),
+    url(r'^(?P<book_id>[0-9]+)', views.edit, name='edit'),
 ]
--- data/views.py
+++ data/views.py
@@ -1,17 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Series, Book, Author
 from django.http import HttpResponse
 from django.template import loader
 
+
 # Create your views here.
-def index(request):
-    return HttpResponse("data shit")
+def add(request):
+    return render(request, 'data/add.html')
+
+
+def edit(request, book_id):
+    book=get_object_or_404(Book, pk=book_id)
+    return render(request, 'data/edit.html', {'book': book})
 
 
 
 #literally copied off of starting tutorial.  trying to get list of books by title show
-def sort_by_book(request):
-    book_list = Book.objects.filter(Book.title)
-    template = loader.get_template('templates/data/index.html')
-    context = {'book': Book.title}
-    return render(request, 'data/sort_by_book.html', context)
+#def sort_by_book(request):
+#    book_list = Book.objects.filter(Book.title)
+#    template = loader.get_template('templates/data/index.html')
+#    context = {'book': Book.title}
+#    return render(request, 'data/sort_by_book.html', context)
