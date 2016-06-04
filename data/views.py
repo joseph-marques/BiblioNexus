414efc002912c8487de6d40064bf0b6ab53b7a38
bschlur <bschlur@gmail.com>
2016-06-04 00:47:14 -0400

    Added method to data/views.py that might return list of books, idk if itll work
    Fixed some formatting in sort_by_book that python would bitch about, added fresh template index.html file, added template imports to data/views

--- data/views.py
+++ data/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from .models import Series, Book, Author
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
     return HttpResponse("data shit")
 
+
+
 #literally copied off of starting tutorial.  trying to get list of books by title show
 def sort_by_book(request):
     book_list = Book.objects.filter(Book.title)
-    context = {'book':Book.title}
-    return render(request, 'data/sort_by_book.html',context)
+    template = loader.get_template('templates/index.html')
+    context = {'book': Book.title}
+    return render(request, 'data/sort_by_book.html', context)
